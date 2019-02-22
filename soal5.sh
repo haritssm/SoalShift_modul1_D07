cdf15bc016b225f48a72d412b7e1ecae15b04140
haritssm <38557477+haritssm@users.noreply.github.com>
2019-02-22 17:04:46 +0800

    Update soal5.sh

--- soal5.sh
+++ soal5.sh
@@ -1,1 +1,1 @@
-awk '{if($0 ~ /cron/ && $0 !~ /sudo/ && NF < 13) print $0}' /var/log/syslog > /home/gede/modul1/soal5.log
+awk '{if($0 !~ /sudo/ && $0 ~ /cron/ && NF < 13) print $0}' /var/log/syslog > /home/haritssm/modul1/soal5.log
